59f4f742849682781a746ffea96cc34914aade2e
mallolumanishbabu <mallolumanishbabu@gmail.com>
2026-06-23 22:20:18 +0530

    fix model import case

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.get("/api/products", (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
