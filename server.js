35c6bd001450d0bd02463e4b966ca8c04818e6a0
mallolumanishbabu <mallolumanishbabu@gmail.com>
2026-06-25 11:33:34 +0530

    fix express cors

--- server.js
+++ server.js
@@ -8,29 +8,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const allowedOrigins = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL.split(",").map((url) => url.trim())
-  : [
-      "http://localhost:5173",
-      "https://codevector-frontend-kdnur2c4e-manish0437s-projects.vercel.app",
-    ];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://codevector-frontend-kdnur2c4e-manish0437s-projects.vercel.app",
+];
 
 console.log("Allowed Origins:", allowedOrigins); // Debug log
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      const allowed = [
-        "http://localhost:5173",
-        "https://codevector-frontend-phi.vercel.app",
-        "https://codevector-frontend-kdnur2c4e-manish0437s-projects.vercel.app",
-      ];
-
-      if (!origin || allowed.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
+    origin: (origin, callback) => {
+      // allow Postman / server requests
+      if (!origin) {
+        return callback(null, true);
       }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("CORS blocked"));
     },
 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -40,48 +37,44 @@ app.use(
     credentials: true,
   }),
 );
-app.options("*", cors());
-// Middleware
+
+// Express json middleware
+
 app.use(express.json());
+
+// =====================
+// DATABASE
+// =====================
 
 const PORT = process.env.PORT || 3000;
 
 const dbUrl = process.env.MONGODB_URL;
+
 if (!dbUrl) {
-  console.error("MONGODB_URL is not defined in environment variables");
+  console.log("MONGODB_URL missing");
+
   process.exit(1);
 }
+
 mongoose
-  .connect(dbUrl, {
-    retryWrites: true,
-    w: "majority",
-  })
-  .then((result) => {
+  .connect(dbUrl)
+  .then(() => {
+    console.log("MongoDB connected");
+
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server running on ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error("MongoDB connection error:", error);
+    console.log("MongoDB error:", error);
   });
 
-// Test Route
+// =====================
+// ROUTES
+// =====================
+
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
 app.use("/products", productRoutes);
-
-app.get("/api/products", (req, res) => {
-  Product.find()
-    .then((products) => {
-      res.json(products);
-    })
-    .catch((error) => {
-      console.error("Error fetching products:", error);
-      res.status(500).json({ error: "Internal server error" });
-    });
-});
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
